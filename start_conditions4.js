0a8c76026d82662ced8c19162b66c421891f58a8
mk014070 <mk014070@gmail.com>
2024-03-23 21:57:38 +0530

    my commit

---
+++ start_conditions4.js
@@ -0,0 +1,25 @@
+// *       *
+// **     **
+// ***   ***
+// **** ****
+// *********
+
+const n = 5;
+for (var i = 1; i <= n; i++){
+    let x = i%5;
+    if (x == 1) {
+        console.log(`${"*".repeat(1)}${" ".repeat(7)}${"*".repeat(1)}`);
+    }
+    else if (x == 2) {
+        console.log(`${"*".repeat(2)}${" ".repeat(5)}${"*".repeat(2)}`);
+    }
+    else if (x == 3) {
+        console.log(`${"*".repeat(3)}${" ".repeat(3)}${"*".repeat(3)}`);
+    } 
+    else if (x == 4){
+        console.log(`${"*".repeat(4)}${" ".repeat(1)}${"*".repeat(4)}`);
+    }
+    else {
+        console.log(`${"*".repeat(9)}`);
+    }
+}
--- switch_conditions3.js
+++ switch_conditions3.js
@@ -2,7 +2,7 @@
 //   ***
 //  *****
 // *******
-const n = 4;
+const n = 8;
 for (var i = 1; i <= n; i++) {
     const x = i%4;
     switch (x) {
@@ -20,3 +20,17 @@ for (var i = 1; i <= n; i++) {
              break;            
     }
 }
+
+const list = [1,2,3,4,5];
+
+let x1 = 0;
+const y1 = 0;
+list.forEach(element => {
+    let x = element;
+    const y = element;
+    y = 10
+    x1 = element;
+    // y1 = element;
+    console.log(x,"first plcace", y,x1);
+});
+// console.log("first plcace",x1, y1);
