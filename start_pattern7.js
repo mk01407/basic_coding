4f8d9c280a35cf1185ddac1808f5cf0f8a0ae729
mk014070 <mk014070@gmail.com>
2024-03-26 09:15:11 +0530

    my commit

--- start_pattern7.js
+++ start_pattern7.js
@@ -4,8 +4,9 @@
 // **** ****
 // *********
 
-const n = 3;
-for (var i = 0; i <= n; i++) {
-    console.log("*".repeat(i+1)," ".repeat((n-i)*2+1),"*".repeat(i+1));
-
+const n = 5;
+for (var i = 0; i < n; i++) {
+    console.log(`${"*".repeat(i+1)}${" ".repeat((n-i-1)*2)}${"*".repeat(i+1)}`);
 }
+// console.log(`${"*".repeat(n*2+1)}`);
+
---
+++ start_pattern8.js
@@ -0,0 +1,12 @@
+//     *       *
+//    ***     ***
+//   *****   *****
+//  ******* *******
+// *****************
+
+// Solve this with simple for loop iteration
+
+const n = 5;
+for (var i = 0; i <= n; i++) {
+    console.log(`${" ".repeat(n-i)}${"*".repeat(2*i+1)}${" ".repeat((n-i)*2)}${"*".repeat(2*i+1)}`);
+}
