694c17a5f2d3a0d21324f13e7912565dbf44073b
mk014070 <mk014070@gmail.com>
2024-04-14 08:24:12 +0530

    my commit

--- function/function.js
+++ function/function.js
@@ -133,16 +133,16 @@ const startpatern_7 = async (n, i) => {
 
 const top_start_pattern_8 = async (n, i) => {
     if (i < n) {
-        console.log(`${" ".repeat(n-i-1)}${"*".repeat(2*i+1)}${" ".repeat((n-i-1)*2)}${"*".repeat(2*i+1)}`);
+        console.log(`${" ".repeat(n - i - 1)}${"*".repeat(2 * i + 1)}${" ".repeat((n - i - 1) * 2)}${"*".repeat(2 * i + 1)}`);
         i += 1;
-       top_start_pattern_8(n,i);
+        top_start_pattern_8(n, i);
     } else {
         // console.log(`${"*".repeat(n*4)}`);
-        bottom_start_pattern_8(n,1);
+        bottom_start_pattern_8(n, 1);
     }
 }
 const bottom_start_pattern_8 = async (n, i) => {
-    console.log(`${" ".repeat(i)}${"*".repeat(2*(n-i)-1)}${" ".repeat(2*i)}${"*".repeat(2*(n-i)-1)}`);
+    console.log(`${" ".repeat(i)}${"*".repeat(2 * (n - i) - 1)}${" ".repeat(2 * i)}${"*".repeat(2 * (n - i) - 1)}`);
     i += 1;
     if (i < n) {
         bottom_start_pattern_8(n, i);
@@ -157,11 +157,11 @@ const bottom_start_pattern_8 = async (n, i) => {
 // **** *****
 // ***** *****
 
-const start_pattern_9 = async (n,i) => {
-    console.log(`${"*".repeat(i)} ${"*".repeat(n)} ${"*".repeat(n-i)}`);
+const start_pattern_9 = async (n, i) => {
+    console.log(`${"*".repeat(i)} ${"*".repeat(n)} ${"*".repeat(n - i)}`);
     i += 1;
     if (i <= n) {
-        start_pattern_9(n,i);   
+        start_pattern_9(n, i);
     }
 }
 
@@ -175,15 +175,15 @@ const start_pattern_9 = async (n,i) => {
 // *****
 //   *
 
-const start_condition_pattern_1 = async (n,i) => {
-    if (i%2 == 0) {
+const start_condition_pattern_1 = async (n, i) => {
+    if (i % 2 == 0) {
         console.log(`${" ".repeat(2)}${"*".repeat(1)}`);
     } else {
         console.log(`${"*".repeat(5)}`);
     }
     i += 1;
-    if (i <= n ) {
-        start_condition_pattern_1(n,i);
+    if (i <= n) {
+        start_condition_pattern_1(n, i);
     }
 }
 
@@ -201,10 +201,10 @@ const start_condition_pattern_1 = async (n,i) => {
 // Fibonacci series
 // Eg (0, 1, 1, 2, 3, 5, 8, 13, 21, 34.........)
 
-const fibonacci = (n,i) => {
+const fibonacci = (n, i) => {
 
     fristvalue = i;
-    secondvalue = i+1;
+    secondvalue = i + 1;
     console.log(fristvalue);
     console.log(secondvalue);
 
@@ -213,17 +213,17 @@ const fibonacci = (n,i) => {
         const series = fristvalue + secondvalue
         console.log(series);
         fristvalue = secondvalue
-        secondvalue =  series
+        secondvalue = series
 
-    } 
+    }
 
 
 }
 
 
 const table = (tableOf, n) => {
-    for (let x = 1; x <= n; x++ ) {
-        const multiplication = x*tableOf
+    for (let x = 1; x <= n; x++) {
+        const multiplication = x * tableOf
         console.log(`${tableOf} * ${x} = ${multiplication}`);
     }
 }
@@ -233,22 +233,71 @@ const table = (tableOf, n) => {
 // Eg: 4 => 1*2*3*4 = 24
 // Eg: 3 => 1*2*3 = 6
 
-const factorial = (currentvalue, value) => {
-    for (let i = 1; i <= value; i++) {
-         currentvalue = currentvalue * i; 
-    }
-      console.log("factorialvalue", currentvalue)
+const findprime = async (value) => {
+    let prime = true
+    if (value == 1) {
+        console.log("its a prime number")
+    } else {
+        for (let i = 2; i <= value; i++) {
+            if (i != value) {
+                if (value % i == 0) {
+                    prime = false
+                    console.log(value % i)
+                }
+            }
+        }
+        if (prime == true) {
+            console.log("its a prime number")
+        } else {
+            console.log("its not a prime number")
+        }
 
+
+    }
+}
+const primelist = async (startvalue, endvalue) => {
+
+    //    tumhe yaha par prime value dhoodhni h 0 se 100 k beech me jo bhi pade
 }
 
 
+// start value (1) and incrrement value (2) till 100
+// 1, 3, 5, 7, 9, 11, 13 ......
+
+// start value (3) and incrrement value (5) till 100
+// 3, 8, 13, 18, 23 .......
+
+const apseries = async (fristvalue, difference) => {
+
+    for (let x = fristvalue; x < 100; x += difference) {
+        ``
+        console.log(x)
+    }
+}
+
+// start value (1) and incrrement value by(sum of last and 2nd last value) till 100
+// 1, 3, 4, 7, 11, 18, 29, 47,   ......
+
+// start value (1) and incrrement value by(sum of last and 3rd last value) till 100
+// 1, 3, 5, 6, 9, 14, 20, 29,   ......
+
+const apseries1 = async (x,n) => {
+    fristvalue = x;
+    secondvalue = x + 2;
+    console.log(fristvalue);
+    console.log(secondvalue);
+
+    for (let x = 1; x < n; x++) {
+        const series = fristvalue + secondvalue;
+        console.log(series)
+        fristvalue = secondvalue
+        secondvalue = series
+    }
+}
 
 
-
-
-
-const n = 5
-const i = 1
+const n = 8
+const x = 1
 // how we can call function
 // startPatern_1_demo();
 // startPatern_1(n,i);
@@ -262,6 +311,10 @@ const i = 1
 // start_pattern_9(n,i);
 // start_condition_pattern_1(n,i); 
 
-// fibonacci(n,i);
+//  fibonacci(n,i);
 // table(8, 10);
-factorial (1,5);
+// factorial (1,5);
+// findprime(12);
+//  primelist(1, 11);
+// apseries(1,2);
+apseries1(x , n)
---
+++ pratics_coding.js
@@ -0,0 +1,172 @@
+var variable1 = 3;
+let variable2 = 6;
+const variable3 = 8;
+
+variable1 += 1;
+variable2 += 1;
+// variable3 += 1;
+console.log(variable1,variable2,variable3)
+
+var variable5 = 3;
+let variable6 = 6;
+const variable8 = 8;
+
+variable1 -= 1;
+variable2 -= 1;
+// variable3 -= 1;
+console.log(variable5,variable6, variable8)
+
+var variable9 = 3;
+let variable10 = 6;
+const variable11 = 8;
+
+variable9 *= 1;
+variable10 *= 1;
+// variable11 *= 1;
+console.log(variable9,variable10,variable11)
+
+// *
+// **
+// ***
+// ****
+// *****
+
+const n = 5;
+for (var i = 1; i <= n; i++) {
+    console.log("*".repeat(i))
+}
+
+
+// *****
+// ****
+// ***
+// **
+// *
+
+const m = 7;
+for (var i = 1; i <= m; i++) {
+    console.log("*".repeat(m-i))
+}
+
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+
+const j = 5;
+for (var i = 0; i <= j; i++) {
+    console.log(" ".repeat(j-i), "*".repeat(2*i+1))
+}
+
+
+
+
+// *********
+//  *******
+//   *****
+//    ***
+//     *
+
+
+const q = 5;
+for (var i = 1; i <= q; i++) {
+    console.log(" ".repeat(i), "*".repeat((q-i)*2+1))
+}
+
+
+
+// *****^*****
+// ****^^^****
+// ***^^^^^***
+// **^^^^^^^**
+// *^^^^^^^^^*
+
+const w = 5;
+for (var i = 0; i < w; i++) {
+    console.log(`${"*".repeat(w-i)}${"^".repeat(2*i+1)}${"*".repeat(w-i)}`)
+}
+
+
+
+
+// *****^*****
+// ****^^^****
+// ***^^^^^***
+// **^^^^^^^**
+// *^^^^^^^^^*
+// ^^^^^^^^^^^
+// **^^^^^^^**
+// ***^^^^^***
+// ****^^^****
+// *****^*****
+
+
+const e = 5;
+for (var i = 0; i <= e; i++) {
+    console.log(`${"*".repeat(e-i)}${"^".repeat(2*i+1)}${"*".repeat(e-i)}`)
+}
+for (var i = 1; i < e; i++) {
+    console.log(`${"*".repeat(i+1)}${"^".repeat((e-i-1)*2+1)}${"*".repeat(i+1)}`)
+}
+
+
+// *       *
+// **     **
+// ***   ***
+// **** ****
+// *********
+
+const r = 5;
+for (var i = 1; i <= r; i++) {
+    console.log(`${"*".repeat(i)}${" ".repeat((r-i)*2+1)}${"*".repeat(i)}`)
+}
+
+
+
+//     *       *
+//    ***     ***
+//   *****   *****
+//  ******* *******
+// *****************
+
+
+const t = 5;
+for (var i = 0; i < t; i++) { 
+    console.log(`${" ".repeat(t-i)}${"*".repeat(2*i+1)}${" ".repeat((t-i-1)*2)}${"*".repeat(2*i+1)}`)
+}
+
+
+
+
+//     *       *
+//    ***     ***
+//   *****   *****
+//  ******* *******
+// *****************
+//  ******* *******
+//   *****   *****
+//    ***     ***
+//     *       *
+
+const y = 5;
+for (var i = 0; i <= y; i++) {
+    console.log(`${" ".repeat(y-i)}${"*".repeat(2*i+1)}${" ".repeat((y-i)*2)}${"*".repeat(2*i+1)}`)
+}
+for (var i = 1; i <= y; i++) {
+    console.log(`${" ".repeat(i)}${"*".repeat((n-i)*2+1)}${" ".repeat(2*i)}${"*".repeat((n-i)*2+1)}`)
+}
+
+
+// ***** *****
+//  ***** ****
+// * ***** ***
+// ** ***** **
+// *** ***** *
+// **** *****
+// ***** *****
+
+const u = 5; 
+for (var i = 1; i <= u; i++) {
+    console.log("*".repeat(i), "*".repeat(n), "*".repeat(n-i))
+}
