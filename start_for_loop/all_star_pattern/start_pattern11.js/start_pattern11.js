b7900bd26cd070a3ce535f426f1fdf718a35596c
mk014070 <mk014070@gmail.com>
2024-05-02 14:13:58 +0530

    my commit

--- start_for_loop/all_star_pattern/start_pattern11.js/start_pattern11.js
+++ start_for_loop/all_star_pattern/start_pattern11.js/start_pattern11.js
@@ -1,10 +1,10 @@
 //     *
-//    * *
-//   *   *
-//  *     *
+//    ***
+//   *****
+//  *******
 // *********
 
-const n = 5;
+const n = 5; 
 for (var i = 0; i <= n; i++) {
     console.log(" ".repeat(n-i), "*".repeat(2*i+1))
 }
